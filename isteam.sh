8c158a90ff504335e5a611a0803ae38441d0d164
Ƭɧℰ-ℜãψ <jasht.sserie@gmail.com>
2016-08-23 08:16:41 +0000

    Update isteam.sh

--- isteam.sh
+++ isteam.sh
@@ -8,5 +8,12 @@ tar -xf steamcmd_linux.tar.gz
 rm -Rf steamcmd_linux.tar.gz
 cd ~/steamcmd
 ./steamcmd.sh +login anonymous +force_install_dir /home/$user/cstrike/ +app_update 90 +app_update 90 +app_update 90 +quit
-quit
 clear
+ls -la /home/`whoami`/cstrike/
+echo "*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*"
+echo "*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*"
+echo "Counter Strike 1.6 Is Done."
+echo "Now Installing Amxmod, dproto, metamod."
+echo "*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*"
+echo "*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*"
+cd /home/`whoami`/cstrike/cstrike
