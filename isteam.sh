8edb69a3f96c661b1908d4895315f42e4e7e031c
Ƭɧℰ-ℜãψ <jasht.sserie@gmail.com>
2016-08-23 08:01:04 +0000

    Update isteam.sh

--- isteam.sh
+++ isteam.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 # Created For Users With No SUDO Access!
+user=$(whoami)
 mkdir ~/steamcmd
 cd ~/steamcmd
 wget https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz
 tar -xf steamcmd_linux.tar.gz
 rm -Rf steamcmd_linux.tar.gz
 cd ~/steamcmd
-./steamcmd.sh
-login anonymous
-force_install_dir /home/steam/cstrike/
-app_update 90
-app_update 90
-app_update 90
+./steamcmd +login anonymous +force_install_dir /home/$user/cstrike/ +app_update 90 +app_update 90 +app_update 90 +quit
 quit
-exit
 clear
