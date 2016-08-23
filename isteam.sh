65266b6b6ea59c7e27fe6f478a70a44f439be68e
Ƭɧℰ-ℜãψ <jasht.sserie@gmail.com>
2016-08-23 08:04:26 +0000

    Update isteam.sh

--- isteam.sh
+++ isteam.sh
@@ -7,6 +7,6 @@ wget https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz
 tar -xf steamcmd_linux.tar.gz
 rm -Rf steamcmd_linux.tar.gz
 cd ~/steamcmd
-./steamcmd +login anonymous +force_install_dir /home/$user/cstrike/ +app_update 90 +app_update 90 +app_update 90 +quit
+./steamcmd.sh +login anonymous +force_install_dir /home/$user/cstrike/ +app_update 90 +app_update 90 +app_update 90 +quit
 quit
 clear
